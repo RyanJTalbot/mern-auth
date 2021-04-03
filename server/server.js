0247f77de6dd4f6d951215576415e367c4fec8a8
Ryan JT <ryantalbot7@gmail.com>
2021-04-03 17:44:49 -0500

    Prep for deployment

--- server/server.js
+++ server/server.js
@@ -76,17 +76,16 @@ app.use('/mongos', mongoRouter);
 // Connect to Mongo
 app.use('/nodes', nodeRouter);
 
+// for production
+
+app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
+
 if (process.env.NODE_ENV === 'production') {
 	app.use(express.static(path.join(__dirname, '/frontend/build')));
 
 	app.get('*', (req, res) =>
 		res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html')),
 	);
-}
-
-// if in production
-if (process.env.NODE_ENV === 'production') {
-	app.use(express.static('frontend/build'));
 }
 
 app.listen(PORT, () => console.log(`Server up and running on port ${PORT} !`));
